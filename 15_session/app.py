e4e2791ab7aa8c0b730e519af42c4cf377cb81bb
Sadid-Ethun <ethun2004@gmail.com>
2021-10-19 23:50:47 -0400

    Changed request methods to POST and removed fail.html

--- 15_session/app.py
+++ 15_session/app.py
@@ -2,6 +2,7 @@
 # SoftDev
 # K15 -- Sessions Greetings / Flask Form and Error Handling
 # 2021-10-18
+import os
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -11,39 +12,39 @@ from flask import session
 #from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
-app.secret_key= "unguessable" #pretty good huh
+app.secret_key= "stuff"  #pretty good huh
 
 #only current login is sethun20 with password set as hello
 user = 'sethun20'
 pwd = 'hello'
 
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/" , methods=['GET', 'POST']) 
 def disp_loginpage():
-    #sends to welcome page if user is signed in
-    if session['username']:
-        return render_template('welcome.html', username=session['username'], method=request.method,
+    #sends to response page if user is signed in
+    if session:
+        return render_template('response.html', username=session['username'], method=request.method,
                                password=session['password'])
     #else sends to login page
     return render_template( 'login.html' )
 
-
-@app.route("/auth") # , methods=['GET', 'POST'])
+@app.route("/auth" , methods=['GET', 'POST']) 
 def authenticate():
     #signs in if password is right
-    if (request.args['username'] == user and request.args['password'] == pwd):
-        session['username']=request.args['username']
-        session['password'] = request.args['password']
-        return render_template( 'welcome.html', username=session['username'], method=request.method, password=session['password']) #response to a form submission
+    if (request.form['username'] == user and request.form['password'] == pwd):
+        session['username']=request.form['username']
+        session['password'] = request.form['password']
+        return render_template( 'response.html', username=session['username'], method=request.method, password=session['password']) #response to a form submission
 
     #both redirect to error pages
-    elif (request.args['username'] != user):
-        return render_template('fail.html', k="Username does not exist")
+    elif (request.form['username'] != user):
+        return render_template('login.html', k="Username does not exist")
     else:
-        return render_template('fail.html', k="Incorrect Password for "+request.args['username'])
-@app.route('/logout')
+        return render_template('login.html', k="Incorrect Password for "+request.form['username'])
+
+@app.route('/logout' , methods=['GET', 'POST']) 
 #clears session
 def logout():
-    session.pop('username');
+    session.pop('username')
     session.pop('password')
     return render_template('login.html')
 
