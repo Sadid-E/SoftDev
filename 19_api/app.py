71346f059424025ac1d033e38b7e311f6a3278df
Sadid Ethun <sethun@Sadids-MacBook-Air.local>
2021-11-23 14:36:13 -0500

    Added explanation

--- 19_api/app.py
+++ 19_api/app.py
@@ -8,8 +8,9 @@ app = Flask(__name__) #create instance of class Flask
 def image():
     r = requests.get("https://api.nasa.gov/planetary/apod?api_key=CpxWFgxsaXpvNJ0ReQFhG7LpydiwYW569yQljz9p")
     url = r.json()["url"]
+    ex = r.json()["explanation"]
     print(url)
-    return render_template("main.html", image=url)
+    return render_template("main.html", image=url, explanation=ex)
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
