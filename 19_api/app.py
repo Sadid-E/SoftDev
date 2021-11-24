a7c847f5255eae58428f834de0c051882d3c93b5
Sadid Ethun <sethun@sadids-air.myfiosgateway.com>
2021-11-23 23:52:16 -0500

    Used urlib3

--- 19_api/app.py
+++ 19_api/app.py
@@ -6,17 +6,21 @@
 
 from flask import Flask
 from flask import render_template
-import requests
+import urllib3
+import json
 
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")       #assign fxn to route
 def image():
-    r = requests.get("https://api.nasa.gov/planetary/apod?api_key=CpxWFgxsaXpvNJ0ReQFhG7LpydiwYW569yQljz9p")
-    url = r.json()["url"]
-    ex = r.json()["explanation"]
-    print(url)
-    return render_template("main.html", image=url, explanation=ex)
+    http = urllib3.PoolManager()
+    resp = http.request("GET","https://api.nasa.gov/planetary/apod?api_key=CpxWFgxsaXpvNJ0ReQFhG7LpydiwYW569yQljz9p")
+    d = json.loads(resp.data)
+    print(d)
+    img = d["url"]
+    print(img)
+    ex = d["explanation"]
+    return render_template("main.html", image=img, explanation=ex)
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
