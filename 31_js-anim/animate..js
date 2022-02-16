cdb6606ffe9c0e98009b2338ba536622461f6999
Sadid Ethun <sethun@sadids-air.myfiosgateway.com>
2022-02-15 21:16:16 -0500

    Added animate

---
+++ 31_js-anim/animate..js
@@ -0,0 +1,85 @@
+// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon
+// SoftDev pd0
+// K31 -- canvas based JS animation
+// 2022-02-15t
+
+// model for HTML5 canvas-based animation
+
+// SKEELTON
+
+
+//access canvas and buttons via DOM
+var c = document.getElementById("playground");
+var dotButton = document.getElementById("buttonCircle");
+var stopButton = document.getElementById("buttonStop");
+
+//prepare to interact with canvas in 2D
+var ctx = c.getContext("2d");
+
+//set fill color to team color
+ctx.fillStyle = "purple";
+
+var requestID;  //init global var for use with animation frames
+
+
+//var clear = function(e) {
+var clear = (e) => {
+  console.log("clear invoked...")
+   
+  // YOUR CODE HERE
+  ctx.clearRect(0, 0, c.width, c.height)
+};
+
+
+var radius = 0;
+var growing = true;
+
+
+//var drawDot = function() {
+var drawDot = () => {
+    console.log("drawDot invoked...")
+
+  // YOUR CODE HERE
+    if (radius > c.width/2) {
+        growing = false;
+    }
+    while(growing){
+        ctx.beginPath();
+        ctx.arc(c.width/2, c.height/2, radius, 0, 360);
+        ctx.fill();
+        radius++;
+        clear();
+    }
+  /*
+    ...to
+    Wipe the canvas,
+    Repaint the circle,
+
+    ...and somewhere (where/when is the right time?)
+    Update requestID to propagate the animation.
+    You will need
+    window.cancelAnimationFrame()
+    window.requestAnimationFrame()
+
+   */
+};
+
+
+//var stopIt = function() {
+var stopIt = () => {
+  console.log("stopIt invoked...")
+  console.log( requestID );
+
+  // YOUR CODE HERE
+  /*
+    ...to
+    Stop the animation
+
+    You will need
+    window.cancelAnimationFrame()
+  */
+};
+
+
+dotButton.addEventListener( "click", drawDot );
+stopButton.addEventListener( "click",  stopIt );
